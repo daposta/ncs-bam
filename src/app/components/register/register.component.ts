107a8c309214d56ae6abc4fec68fd5e878a94fc8
Oladapo LAWORE <dapolawore@gmail.com>
2017-11-15 12:31:20 +0100

    user registration done

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+
+declare var $: any;
 
 import { UsersService} from '../../services/users.service';
 @Component({
@@ -7,21 +9,57 @@ import { UsersService} from '../../services/users.service';
   styleUrls: ['./register.component.css'],
   providers : [UsersService, ]
 })
+
 export class RegisterComponent implements OnInit {
+
 
   newUser: any= {};
   error: any;	
+  users: any[];
+  //formData: FormData;
 
-  constructor(private userSrv: UsersService) { }
+  constructor(private userSrv: UsersService) {
+   }
 
   ngOnInit() {
+    console.log('skrraaaa');
+    this.fetchUsers();
+
   }
 
   register(){
-    this.newUser.userid ="zumba";
-    this.newUser.isaccountverified = 0;
-    this.newUser.lastlogindate = "2017-11-14T13:55:18.223Z";
-  	this.userSrv.register(this.newUser);
+    if(!localStorage.getItem('uid')){
+       this.newUser.UserId ="219";
+     }else{
+       this.newUser.UserID = localStorage.getItem('uid');
+     }
+     console.log(this.newUser.UserID);
+   
+
+    let formData = new FormData();
+    formData.append('FirstName', this.newUser['FirstName']);
+    formData.append('LastName', this.newUser['LastName']);
+    formData.append('Phone', this.newUser['Phone']);
+    formData.append('Email' ,this.newUser['FirstName']);
+    formData.append('Password', this.newUser['Password']);
+    formData.append('UserId' , this.newUser['UserId']);
+    // formData.append('UserId', '911');
+    //   formData.append('Email', 'leke@me.com');
+    //   formData.append('Phone', '07066655543');
+    //   formData.append('FirstName', 'Leke');
+    //   formData.append('LastName', 'Alao');
+    //   formData.append('Password', 'errytr-=ouokj');
+    console.log(formData);
+    localStorage.setItem('uid',  this.newUser.UserId+1 )
+    //this.newUser['ConfirmPassword'] = '';
+    this.userSrv.register(formData);
+  }
+
+
+  fetchUsers(){
+    this.userSrv.fetchUsers()
+            .then(res => this.users = res)
+            .catch(error => this.error = error);
   }
 
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { Http, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 import {Router} from '@angular/router';
 // import {ToasterContainerComponent, ToasterService} from 'angular2-toaster';
 // import {BodyOutputType} from 'angular2-toaster';
+import 'rxjs/add/operator/toPromise';
+declare var $: any;
 
 @Injectable()
 export class UsersService {
 	evil : any;
   // private loginUrl = this.globals.LOGIN_URL; 
-  private registerUrl = "https://129.144.154.136/ords/pdb1/ncs/system/users/";
+  private usersUrl = "https://129.144.154.136/ords/pdb1/ncs/system/users/";
   constructor(private http: Http, private router:Router,) { }
 
  //  login(mobile: string, password : string){
@@ -45,35 +47,70 @@ export class UsersService {
 	// };
 
 
-	register(data: any){
-		return this.http.post(this.registerUrl,data)
-		.subscribe(res =>{
-				// this._toasterService.pop('success', 'Registration Successful', );
-				//localStorage.setItem('mobile' , data['mobile']);
-				//this.router.navigateByUrl('/verify-number');
+	fetchUsers(){
 
-				console.log(res);
-				console.log(res.status)
-				if(res.status == 201){
-					this.router.navigateByUrl('login');
-				}
-				
-				
-
-		}, error =>{
-			
-
-			if(error['status']){
-				this.evil = JSON.parse(error['_body']).message; //.non_field_errors[0];
-			//	this._toasterService.pop('error', this.evil, '');
-			}else{
-				//this._toasterService.pop('error', 'You are not connected to the server', '');
-			}
-		   
-			
-		})
+		   //let v = this.page_header();
+		   let headers = new Headers();
+   		 headers.append('Content-Type','application/json');
+   		 headers.append('Allow-Cross-Origin','*');
+		   return this.http.get(this.usersUrl,  {headers: headers})
+		              .toPromise()
+		              .then(response => response.json())
+		              .catch(this.handleError);
 
 	};
+
+
+	register(data: FormData){
+		
+   		
+
+   		 $.ajax ( {
+				type: 'POST',
+				url: this.usersUrl,
+				enctype: ' multipart/form-data',
+				data: data,
+				cache: false,
+				processData: false,
+				contentType: false,
+				crossDomain: true,
+				xhrFields: { withCredentials: true },
+				beforeSend: function (xhr) { 
+					console.log('setting credentials.......');
+					
+				},
+				success: function(data) { 
+					console.log("=====Sent successfully to the database========");
+					window.location.href= '/login';
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log("=====uploading system error ========");
+				}
+			} );
+		
+
+	};
+
+
+
+	private page_header(){
+     //let data =  localStorage.getItem('auth_token');
+      let headers = new Headers();
+      let opt: RequestOptions;
+      //headers.append('Authorization', 'JWT ' + data );
+      opt = new RequestOptions({headers: headers})  ;
+      return opt;
+  }
+
+  private handleError(error: any) {
+    var err = error.json();
+    if(error.detail = "Signature has expired."){
+      localStorage.removeItem('auth_token');
+      this.router.navigateByUrl('/login');
+    }
+    return Promise.reject(error.message || error);
+  }
+
 
 
 
