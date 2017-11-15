ed2d5d5731588517e44d2006827be82514ff394c
Oladapo LAWORE <dapolawore@gmail.com>
2017-11-15 01:24:38 +0100

    fixed css issue

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  
+  bodyClasses:string = "skin-blue sidebar-mini";
   constructor() { }
 
   ngOnInit() {
-      let body = document.getElementsByTagName('body')[0];
+
+     // let body = document.getElementsByTagName("body")[0];
     //  body.classList.remove("className");   //remove the class
-      body.classList.add("skin-blue sidebar-mini");   //add the class
+    //  body.classList.add("skin-blue sidebar-mini");   //add the class
+    document.body.classList.add("skin-blue");
+    document.body.classList.add("sidebar-mini");
   }
 
 }
