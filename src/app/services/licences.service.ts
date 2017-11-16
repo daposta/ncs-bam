727c12f99524e2cc4289a0bff56cd0bd7d3f4e2e
MUSAH\LENOVO <rabiu4love@yahoo.com>
2017-11-16 16:24:58 +0100

    Entry of premise completed

--- src/app/services/licences.service.ts
+++ src/app/services/licences.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, URLSearchParams } from '@angular/http';
 import {Router} from '@angular/router';
 import 'rxjs/add/operator/toPromise';
