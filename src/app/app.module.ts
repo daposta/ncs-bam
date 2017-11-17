ff6987967c3493da4ce3336e652d1660a5aa77a8
Oladapo LAWORE <dapolawore@gmail.com>
2017-11-17 16:19:56 +0100

    prevent user from logging out if session still active

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import {
   AuthGuardService as AuthGuard 
 } from './auth/auth-guard.service';
 
+
+import { 
+  NoAuthGuardService as NoAuthGuard 
+} from './auth/no-auth-guard.service';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewForm41Component } from './components/new-form-41/new-form-41.component';
@@ -46,7 +51,6 @@ import { RegisterDetailComponent } from './components/register-detail/register-d
 import { NewRegisterComponent } from './components/new-register/new-register.component';
 import { NewAssignmentComponent } from './components/new-assignment/new-assignment.component';
 import { AssignmentsListingComponent } from './components/assignments-listing/assignments-listing.component';
-import { EntrysOfPremiseComponent } from './components/cac/entrys-of-premise/entrys-of-premise.component';
 import { CacEntrysOfPremiseComponent } from './components/cac/cac-entrys-of-premise/cac-entrys-of-premise.component';
 import { CacAssignmentsListingComponent } from './components/cac/cac-assignments-listing/cac-assignments-listing.component';
 import { CacNewAssignmentComponent } from './components/cac/cac-new-assignment/cac-new-assignment.component';
@@ -57,8 +61,8 @@ import { CacNewAssignmentComponent } from './components/cac/cac-new-assignment/c
 const appRoutes: Routes = [
   
      { path: '', component: MyProfileComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent , canActivate: [NoAuthGuard]},
+    { path: 'register', component: RegisterComponent, canActivate: [NoAuthGuard] },
      { path: 'register-as-admin', component: RegisterAsAdminComponent, canActivate: [AuthGuard]  },
      { path: 'forgot-password', component: ForgotPasswordComponent },
      { path: 'new-form-41', component: NewForm41Component, canActivate: [AuthGuard]  },
@@ -125,7 +129,6 @@ const appRoutes: Routes = [
     NewRegisterComponent,
     NewAssignmentComponent,
     AssignmentsListingComponent,
-    EntrysOfPremiseComponent,
     CacEntrysOfPremiseComponent,
     CacAssignmentsListingComponent,
     CacNewAssignmentComponent,
@@ -138,7 +141,7 @@ const appRoutes: Routes = [
 
 
   ],
-  providers: [AuthGuard],
+  providers: [AuthGuard, NoAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
