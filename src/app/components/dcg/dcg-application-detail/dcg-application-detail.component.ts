49469ead481c5fa37951dd25902f2a17b45defc9
Oladapo LAWORE <dapolawore@gmail.com>
2017-11-19 04:30:48 +0100

    oco manage applications

--- src/app/components/dcg/dcg-application-detail/dcg-application-detail.component.ts
+++ src/app/components/dcg/dcg-application-detail/dcg-application-detail.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Form41sService} from '../../../services/form41s.service';
 
 @Component({
   selector: 'app-dcg-application-detail',
   templateUrl: './dcg-application-detail.component.html',
-  styleUrls: ['./dcg-application-detail.component.css']
+  styleUrls: ['./dcg-application-detail.component.css'],
+  providers : [Form41sService, ]
+
 })
 export class DcgApplicationDetailComponent implements OnInit {
-
-  constructor() { }
+	
+   application: Object= {};
+ 	error: any;	
+  constructor(private form41Srv: Form41sService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+  		this.getDetail();
   }
+
+
+    getDetail(){
+  	 this.route.params.switchMap((params: Params) => 
+		 	this.form41Srv.findForm41ByID(+ params['id']))
+		 .subscribe(
+		 	data =>this.application = data.items[0]	);
+
+
+  }
+
 
 }
--- src/app/components/dcg/dcg-manage-applications/dcg-manage-applications.component.ts
+++ src/app/components/dcg/dcg-manage-applications/dcg-manage-applications.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Form41sService} from '../../../services/form41s.service';
 
 @Component({
   selector: 'app-dcg-manage-applications',
   templateUrl: './dcg-manage-applications.component.html',
-  styleUrls: ['./dcg-manage-applications.component.css']
+  styleUrls: ['./dcg-manage-applications.component.css'],
+  providers : [Form41sService, ]
+
 })
 export class DcgManageApplicationsComponent implements OnInit {
 
-  constructor() { }
+   applications: any[];
+ 	error: any;
+  constructor(private form41Srv: Form41sService) { }
 
   ngOnInit() {
+  	this.fetchForm41s();
   }
+
+  
+  fetchForm41s(){
+
+  	this.form41Srv.fetchForm41().then(result => this.applications = result.items)
+  	.catch(err => this.error = err);
+  };
 
 }
