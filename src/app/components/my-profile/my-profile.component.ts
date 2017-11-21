8985cbea9c917ee8ceb487a4c1c232301474bd5a
Oladapo LAWORE <dapolawore@gmail.com>
2017-11-21 18:00:54 +0100

    company profile update

--- src/app/components/my-profile/my-profile.component.ts
+++ src/app/components/my-profile/my-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+declare var $: any;
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class MyProfileComponent implements OnInit {
   
    user: any= {};
+   org:any= {};
   constructor() { }
 
   ngOnInit() {
@@ -15,6 +17,39 @@ export class MyProfileComponent implements OnInit {
   		this.user = JSON.parse(localStorage.getItem('user'));
   	}
   	
+
+  }
+
+  companyInfo(){
+\
+    let postInfoTOBCURL= "";
+    let data = {"tin" : this.org.tin,"rcNumber": this.org.rcNumber};
+
+    $.ajax ( {
+              type: 'POST',
+              url: postInfoTOBCURL,
+              enctype: ' multipart/form-data',
+              data: data,
+              cache: false,
+              processData: false,
+              contentType: false,
+              crossDomain: true,
+              xhrFields: { withCredentials: true },
+              beforeSend: function (xhr) { 
+                console.log('setting credentials.......');
+                
+              },
+              success: function(data) { 
+                console.log("=====Sent successfully to the database========");
+
+                toastr.success("Success", 'Upload Successful');
+
+             
+              },
+              error: function(jqXHR, textStatus, errorThrown) {
+                console.log("=====uploading system error ========");
+              }
+            } );
 
   }
 
