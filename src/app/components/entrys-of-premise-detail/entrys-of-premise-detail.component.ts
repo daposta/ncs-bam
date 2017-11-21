75a99fa1b84906d4b937f616ed83324f6377ba59
MUSAH\LENOVO <rabiu4love@yahoo.com>
2017-11-21 23:59:19 +0100

    New bank guarantee

--- src/app/components/entrys-of-premise-detail/entrys-of-premise-detail.component.ts
+++ src/app/components/entrys-of-premise-detail/entrys-of-premise-detail.component.ts
@@ -30,125 +30,11 @@ export class EntrysOfPremiseDetailComponent implements OnInit {
     constructor(private form41Srv: Form41sService, private route: ActivatedRoute, private toastr: ToastsManager,
         private _vcr: ViewContainerRef, private uploadsSrv: UploadsService) {
         this.toastr.setRootViewContainerRef(_vcr);
-<<<<<<< HEAD
     }
 
     ngOnInit() {
         this.getDetail()
 
-=======
-  }
-
-  ngOnInit() {
-  	this.getDetail()
-   
-
-  }
-
-
-  getDetail(){
-  	 this.route.params.switchMap((params: Params) => 
-		 	this.form41Srv.findForm41ByID(+ params['id']))
-		 .subscribe(
-		 	data =>{
-         this.entry_of_premise = data.items[0] ;
-           console.log('======');
-           console.log(this.entry_of_premise);
-           this.fetchImagesForForm41();
-           console.log(this.form41Docs);
-       });
-
-  }
-
-  uploadFile(){
-   //check if dir exists, if not create a new one
-    
-    let searchURL = 'https://documents-gse00012792.documents.us2.oraclecloud.com/documents/api/1.2/folders/search/items?fulltext=';
-    let folderName = 'Form 41';
-    let fullFolderSearchURL = searchURL + folderName;
-
-    var toastr = this.toastr;
-    var filesData = this.form41Uploads;
-   
-    //check if folder already exists
-    var doc = this._doc
-    var form41 = this.entry_of_premise;
-   
-    let username ='bala.gupta';
-    let password ='LifeliKe@6Lamb';
-    event.preventDefault();
-    $.ajax
-        ({
-          type: "GET",
-          url: fullFolderSearchURL,
-          async: false,
-          beforeSend: function (xhr) {
-              xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
-             xhr.setRequestHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-          },
-          success: function (data, status){
-             
-             if(data.totalCount > 0 ){
-                 //upload files to directory
-
-                 let parentFolderID = data.items[0]['id'];
-               
-                 let uploadDirURL = 'https://documents-gse00012792.documents.us2.oraclecloud.com/documents/api/1.2/files/data';
-                 //{"parentID": "FAB43E7945CD74F22A2A8D920CA5E537F54010EF7DE2"}
-                 let postData = new FormData();
-               
-                 postData.append('jsonInputParameters','{"parentID": "F0917D035B670E235B5C7BE50CA5E537F54010EF7DE2"}');
-
-                 postData.append('primaryFile', doc);
-              
-                 $.ajax ( {
-                    type: 'POST',
-                    url: uploadDirURL,
-                    enctype: ' multipart/form-data',
-                    data: postData,
-                    cache: false,
-                    processData: false,
-                    contentType: false,
-                    crossDomain: true,
-                    xhrFields: { withCredentials: true },
-                    beforeSend: function (xhr) { 
-                      console.log('setting credentials.......');
-                         xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
-                          xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-                          
-                    },
-                    success: function(response) { 
-                      console.log("=====Sent successfully========");
-
-                      //send attachment to db
-                      let imageID = response['id'];
-                      let attachementURL ='https://129.144.154.136/ords/pdb1/ncs/system/form41attachment/';
-                      let attachmentData = new FormData();
-                      console.log('===' + form41['idform']);
-                      attachmentData.append('idForm', form41['idform']);
-                      attachmentData.append('image',imageID);
-                      attachmentData.append('title', "*****");
-                      attachmentData.append('description' ,"*****");
-                       attachmentData.append('Status' , "Done");
-
-
-                         $.ajax ( {
-                              type: 'POST',
-                              url: attachementURL,
-                              enctype: ' multipart/form-data',
-                              data: attachmentData,
-                              cache: false,
-                              processData: false,
-                              contentType: false,
-                              crossDomain: true,
-                              xhrFields: { withCredentials: true },
-                              beforeSend: function (xhr) { 
-                                console.log('setting credentials.......');
-                                
-                              },
-                              success: function(data) { 
-                                console.log("=====Sent successfully to the database========");
->>>>>>> d8f4f5af9666a82006c70137c710eeeefc11fe00
 
     }
 
@@ -159,9 +45,7 @@ export class EntrysOfPremiseDetailComponent implements OnInit {
             .subscribe(
             data => {
                 this.entry_of_premise = data.items[0];
-                console.log('======');
-                console.log(this.entry_of_premise);
-                this.fetchImagesForForm41(this.entry_of_premise['idform']);
+                this.fetchImagesForForm41();
                 console.log(this.form41Docs);
             });
 
@@ -231,7 +115,6 @@ export class EntrysOfPremiseDetailComponent implements OnInit {
                                 let imageID = response['id'];
                                 let attachementURL = 'https://129.144.154.136/ords/pdb1/ncs/system/form41attachment/';
                                 let attachmentData = new FormData();
-                                console.log('===' + form41['idform']);
                                 attachmentData.append('idForm', form41['idform']);
                                 attachmentData.append('image', imageID);
                                 attachmentData.append('title', "*****");
@@ -273,67 +156,7 @@ export class EntrysOfPremiseDetailComponent implements OnInit {
 
 
                     }
-<<<<<<< HEAD
                 }
-=======
-                  } );
-
-                
-             }
-          }
-      });
-
-  };
-
-
-  addDocument($event, t){
-  
-
-    let  files = $event.target.files || $event.srcElement.files;
-   //this.form41Uploads.push(files[0]);
-   this._doc = files[0];
-  
-
-  }
-
-  fetchImagesForForm41(){
-    console.log('prrrrr');
-    //let imgs = this.entry_of_premise['ATTACHMENTS'];
-    this.fetchThumbnailsFromIDs(this.entry_of_premise['ATTACHMENTS']);
-
-  }
-
-  fetchThumbnailsFromIDs(imageIDs:any[]){
-    let thumbnails:any[];
-    console.log(imageIDs);
-    let thumbnailURL ='https://documents-gse00012792.documents.us2.oraclecloud.com/documents/api/1.2/files/DA5878F4B1A4FEE0DA334AE40CA5E537F54010EF7DE2/data/thumbnail';
-    imageIDs.forEach(function(item){
-     // thumbnails.push(item.);
-         $.ajax ( {
-              type: 'POST',
-              url: thumbnailURL,
-              enctype: ' multipart/form-data',
-              data: 'attachmentData',
-              cache: false,
-              processData: false,
-              contentType: false,
-              crossDomain: true,
-              xhrFields: { withCredentials: true },
-              beforeSend: function (xhr) { 
-                console.log('setting credentials.......');
-                
-              },
-              success: function(data) { 
-                console.log("=====Sent successfully to the database========");
-                thumbnails.push(data);
-                //toastr.success("Success", 'Upload Successful');
-
-             
-              },
-              error: function(jqXHR, textStatus, errorThrown) {
-                console.log("=====uploading system error ========");
-              }
->>>>>>> d8f4f5af9666a82006c70137c710eeeefc11fe00
             });
 
     };
@@ -349,50 +172,52 @@ export class EntrysOfPremiseDetailComponent implements OnInit {
 
     }
 
-    fetchImagesForForm41(formID: any) {
+    fetchImagesForForm41() {
         console.log('prrrrr');
-        this.uploadsSrv.findDocsbyFormID(formID).then(response => {
-            this.form41Docs = response.items;
-            console.log(this.form41Docs);
-        })
-            .catch(error => this.error = error);
+        //let imgs = this.entry_of_premise['ATTACHMENTS'];
+        if (this.entry_of_premise['ATTACHMENTS']) {
+            this.fetchThumbnailsFromIDs(this.entry_of_premise['ATTACHMENTS']);
+        }
+
 
     }
 
     fetchThumbnailsFromIDs(imageIDs: any[]) {
-        let thumbnails: any[];
-        let thumbnailURL = 'https://documents-gse00012792.documents.us2.oraclecloud.com/documents/api/1.2/files/DA5878F4B1A4FEE0DA334AE40CA5E537F54010EF7DE2/data/thumbnail';
+        let thumbnails = [];
+        console.log(imageIDs);
+        let username = 'bala.gupta';
+        let password = 'LifeliKe@6Lamb';
         imageIDs.forEach(function (item) {
             // thumbnails.push(item.);
+
+            let thumbnailURL = 'https://documents-gse00012792.documents.us2.oraclecloud.com/documents/api/1.2/files/' + item['IMAGE'] + '/data/thumbnail';
+
             $.ajax({
-                type: 'POST',
+                type: 'GET',
                 url: thumbnailURL,
-                enctype: ' multipart/form-data',
-                data: 'attachmentData',
-                cache: false,
-                processData: false,
-                contentType: false,
-                crossDomain: true,
+                async: false,
                 xhrFields: { withCredentials: true },
                 beforeSend: function (xhr) {
                     console.log('setting credentials.......');
+                    xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+                    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 
                 },
                 success: function (data) {
                     console.log("=====Sent successfully to the database========");
+                    //thumbnails.push(data);
 
-                    //toastr.success("Success", 'Upload Successful');
-
+                    $('#ppp').prepend($('<img>', { id: 'theImg', src: data }));
 
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
                     console.log("=====uploading system error ========");
-
                 }
             });
 
-
         });
+
+        console.log(thumbnails);
     }
 
 }
